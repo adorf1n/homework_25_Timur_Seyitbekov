743681b74099d32296700f909cc0466a4284e0d7
ad0rfin <adorfin@list.ru>
2024-07-22 17:26:55 +0600

    1 commit

---
+++ MyLinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class MyLinkedList<T>
+{
+    private MyNode<T> head;
+    private MyNode<T> tail;
+    private int count;
+
+    public int Count => count;
+    public MyNode<T> First => head;
+    public MyNode<T> Last => tail;
+
+    public MyLinkedList()
+    {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    public MyNode<T> Add(T element)
+    {
+        MyNode<T> newNode = new MyNode<T>(element);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.Next = newNode;
+            newNode.Previous = tail;
+            tail = newNode;
+        }
+
+        count++;
+        return newNode;
+    }
+
+    public MyNode<T> Find(T value)
+    {
+        MyNode<T> current = head;
+        while (current != null)
+        {
+            if (current.Value.Equals(value))
+            {
+                return current;
+            }
+            current = current.Next;
+        }
+        return null;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyLinkedList<int> list = new MyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        Console.WriteLine("Count: " + list.Count);
+        Console.WriteLine("First: " + list.First.Value);
+        Console.WriteLine("Last: " + list.Last.Value);
+
+        MyNode<int> node = list.Find(5);
+        if (node != null)
+        {
+            Console.WriteLine("Found: " + node.Value);
+        }
+        else
+        {
+            Console.WriteLine("Value not found");
+        }
+    }
+}
